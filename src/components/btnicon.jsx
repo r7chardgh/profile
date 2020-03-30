d7e9000f68ad371e35c4a9bb266b3cef7e4dc260
R7 <richard.tsang9641@gmail.com>
2020-03-31 02:59:38 +0800

    added new icon animation

--- src/components/btnicon.jsx
+++ src/components/btnicon.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { ReactComponent as Profile } from "../icon/user-circle-regular.svg";
 import { ReactComponent as Skill } from "../icon/laptop-code-solid.svg";
+import { ReactComponent as Experience } from "../icon/exp-icon.svg";
 class BtnIcon extends Component {
   render() {
     const { id } = this.props.option;
@@ -9,6 +10,8 @@ class BtnIcon extends Component {
         return <Profile />;
       case 2:
         return <Skill />;
+      case 3:
+        return <Experience />;
       default:
         return <div>No Icon</div>;
     }
