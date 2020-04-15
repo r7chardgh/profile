2c71ea1d21d1cde17e1598a0ae7799b41ac1ae5f
R7 <richard.tsang9641@gmail.com>
2020-04-16 00:46:58 +0800

    Added a part of data

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -19,15 +19,12 @@ class Info extends Component {
       <div id="aboutme" onScroll={() => scrollIn("aboutme")} className="page">
         <h1 className="page-title">{this.props.option.title}</h1>
         <main className="page-content">
-          <div className="scroll-Target info-wrap scrolled">Richard Tsang</div>
-          <div className="scroll-Target info-wrap scrolled">
-            Graduated Student
-          </div>
-          <div className="scroll-Target info-wrap">Living Place</div>
-          <div className="scroll-Target info-wrap">Language</div>
-          <div className="scroll-Target info-wrap">Introduction</div>
-          <div className="scroll-Target info-wrap">Email</div>
-          <div className="scroll-Target info-wrap">Blog</div>
+    <div className="scroll-Target info-wrap scrolled"><span id="info-tag">#tag: </span><span id="info-data">{this.state.person.name}</span></div>
+          <div className="scroll-Target info-wrap scrolled"><span id="info-tag">#tag: </span><span id="info-data">{this.state.person.living}</span></div>
+          <div className="scroll-Target info-wrap scrolled"><span id="info-tag">#tag: </span><span id="info-data">{this.state.person.intro}</span></div>
+          <div className="scroll-Target info-wrap scrolled"><span id="info-tag">#tag: </span><span id="info-data">{this.state.person.email}</span></div>
+          <div className="scroll-Target info-wrap scrolled"><span id="info-tag">#tag: </span>{this.state.person.langs.map(b=><span key={b.type}>{b.type}:{b.level}</span>)}</div>
+    <div className="scroll-Target info-wrap scrolled"><span id="info-tag">#tag: </span>{this.state.person.blog.map(b=><span key={b.type}>{b.type}:{b.link}</span>)}</div>
 
           <div className="card-wrap">
             <img className="card-image" src="../images/avatar.jpg" alt="card" />
--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 class Skill extends Component {
-  state = {};
+  state = {
+    skill:{
+      cLang:[{type:"Html",level:"familiar"},{type:"JavaScript",level:"familiar"},{type:"CSS",level:"familiar"},{type:"C++",level:"capable"}],
+      tool:[{type:"coding",name:"VSCode"}, {type:"drawing", name:"GIMP"}],
+      comp:[]
+    }
+  };
   render() {
     if (!this.props.option.isSelected) return null;
     return (
