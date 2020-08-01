b50430598a0958756c6d8f302c849ecde8f342a1
R7 <richard.tsang9641@gmail.com>
2020-08-02 00:58:10 +0800

    Finished Contact Page v1

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,22 +1,56 @@
-import React from "react";
-const Contact = (props) => {
-  if (!props.option.isSelected) return null;
-  return (
-    <div id="contact" className="page">
-      <h1 className="page-title load">{props.option.title}</h1>
-      <main className="page-content load">
-        <p className="contactMsg">
-          Whether you have any questions for me or comments about my website,
-          feel free to send me an email:
-        </p>
-        <button type="text" value="richardtcfung@gmail.com" id="myEmail">
-          <span className="copyText">richardtcfung@gmail.com</span>
-        </button>
-        <span className="copyTooltip">Click to Copy!</span>
-        <p className="contactMsg">I am all ears.</p>
-      </main>
-    </div>
-  );
-};
+import React, { Component } from "react";
+
+class Contact extends Component {
+  state = {
+    TextToCopy: "richardtcfung@gmail.com",
+    CopyTextMsg: ["Click me to copy :)", "Copied to your clipboard!"],
+  };
+  handleDisplayMsg = () => {
+    document.getElementById("myEmail").innerHTML = this.state.CopyTextMsg[0];
+  };
+  handleRemoveMsg = () => {
+    document.getElementById("myEmail").innerHTML = this.state.TextToCopy;
+  };
+  handleCopy = () => {
+    const copyInput = document.createElement("input");
+    const copyText = document.getElementById("myEmail");
+    copyInput.value = copyText.value;
+    copyInput.id = "myInput";
+    document.body.appendChild(copyInput);
+    copyInput.select();
+    copyInput.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    document.body.removeChild(copyInput);
+    document.getElementById("myEmail").innerHTML = this.state.CopyTextMsg[1];
+  };
+  render() {
+    if (!this.props.option.isSelected) return null;
+    return (
+      <div id="contact" className="page">
+        <h1 className="page-title load">{this.props.option.title}</h1>
+        <main className="page-content load">
+          <p className="contactMsg">
+            Whether you have any questions for me or comments about my website,
+            feel free to send me an email to let me know:
+          </p>
+          <p className="min-break" />
+          <button
+            className="copyButton"
+            type="text"
+            value="richardtcfung@gmail.com"
+            id="myEmail"
+            onMouseOver={this.handleDisplayMsg}
+            onMouseLeave={this.handleRemoveMsg}
+            onMouseDown={this.handleCopy}
+          >
+            {this.state.TextToCopy}
+          </button>
+          <p className="min-break" />
+          <p className="contactMsg">I am all ears.</p>
+        </main>
+      </div>
+    );
+  }
+}
 
 export default Contact;
