29902d2bd1ee9aa31ec2d586e823c691cf9d5972
richard tsang <richard.tsang9641@gmail.com>
2023-09-24 14:21:57 +0800

    temp push - incomplete work pages and styles

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -4,9 +4,130 @@ function Works() {
   return (
     <>
       <div id="works" className="container">
-      <h1 className="title">My works...</h1>
-        <div className="work-item">
-          <h2 className="work-item__title">scroll-animation-module</h2>
+        <h1 className="title">My works...</h1>
+        <div className="slide">
+          <div className="carousel">
+            <a
+              href="https://r7chardgh.github.io/scroll-animation-module/"
+              target="_blank"
+              className="work-item"
+            >
+              <h2 className="work-item__title">scroll-animation-module</h2>
+              <h2 className="work-item__description">
+                This is my first js module for scrolling animation. By simply
+                adding a specific data attribute to the element, you can see the
+                "fade-in" animation when scrolling. This is my first js module
+                for scrolling animation. By simply adding a specific data
+                attribute to the element, you can see the "fade-in" animation
+                when scrolling.
+              </h2>
+            </a>
+            <a
+              href="https://r7chardgh.github.io/scroll-animation-module/"
+              target="_blank"
+              className="work-item"
+            >
+              <h2 className="work-item__title">scroll-animation-module</h2>
+              <h2 className="work-item__description">
+                This is my first js module for scrolling animation. By simply
+                adding a specific data attribute to the element, you can see the
+                "fade-in" animation when scrolling. This is my first js module
+                for scrolling animation. By simply adding a specific data
+                attribute to the element, you can see the "fade-in" animation
+                when scrolling.
+              </h2>
+            </a>
+            <a
+              href="https://r7chardgh.github.io/scroll-animation-module/"
+              target="_blank"
+              className="work-item"
+            >
+              <h2 className="work-item__title">scroll-animation-module</h2>
+              <h2 className="work-item__description">
+                This is my first js module for scrolling animation. By simply
+                adding a specific data attribute to the element, you can see the
+                "fade-in" animation when scrolling. This is my first js module
+                for scrolling animation. By simply adding a specific data
+                attribute to the element, you can see the "fade-in" animation
+                when scrolling.
+              </h2>
+            </a>
+            <a
+              href="https://r7chardgh.github.io/scroll-animation-module/"
+              target="_blank"
+              className="work-item"
+            >
+              <h2 className="work-item__title">scroll-animation-module</h2>
+              <h2 className="work-item__description">
+                This is my first js module for scrolling animation. By simply
+                adding a specific data attribute to the element, you can see the
+                "fade-in" animation when scrolling. This is my first js module
+                for scrolling animation. By simply adding a specific data
+                attribute to the element, you can see the "fade-in" animation
+                when scrolling.
+              </h2>
+            </a>
+            <a
+              href="https://r7chardgh.github.io/scroll-animation-module/"
+              target="_blank"
+              className="work-item"
+            >
+              <h2 className="work-item__title">scroll-animation-module</h2>
+              <h2 className="work-item__description">
+                This is my first js module for scrolling animation. By simply
+                adding a specific data attribute to the element, you can see the
+                "fade-in" animation when scrolling. This is my first js module
+                for scrolling animation. By simply adding a specific data
+                attribute to the element, you can see the "fade-in" animation
+                when scrolling.
+              </h2>
+            </a>
+            <a
+              href="https://r7chardgh.github.io/scroll-animation-module/"
+              target="_blank"
+              className="work-item"
+            >
+              <h2 className="work-item__title">scroll-animation-module</h2>
+              <h2 className="work-item__description">
+                This is my first js module for scrolling animation. By simply
+                adding a specific data attribute to the element, you can see the
+                "fade-in" animation when scrolling. This is my first js module
+                for scrolling animation. By simply adding a specific data
+                attribute to the element, you can see the "fade-in" animation
+                when scrolling.
+              </h2>
+            </a>
+            <a
+              href="https://r7chardgh.github.io/scroll-animation-module/"
+              target="_blank"
+              className="work-item"
+            >
+              <h2 className="work-item__title">scroll-animation-module</h2>
+              <h2 className="work-item__description">
+                This is my first js module for scrolling animation. By simply
+                adding a specific data attribute to the element, you can see the
+                "fade-in" animation when scrolling. This is my first js module
+                for scrolling animation. By simply adding a specific data
+                attribute to the element, you can see the "fade-in" animation
+                when scrolling.
+              </h2>
+            </a>
+            <a
+              href="https://r7chardgh.github.io/scroll-animation-module/"
+              target="_blank"
+              className="work-item"
+            >
+              <h2 className="work-item__title">scroll-animation-module</h2>
+              <h2 className="work-item__description">
+                This is my first js module for scrolling animation. By simply
+                adding a specific data attribute to the element, you can see the
+                "fade-in" animation when scrolling. This is my first js module
+                for scrolling animation. By simply adding a specific data
+                attribute to the element, you can see the "fade-in" animation
+                when scrolling.
+              </h2>
+            </a>
+          </div>
         </div>
       </div>
     </>
